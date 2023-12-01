55a0f13471d2db93e45c8312d38572e482bbd011
EknoorS <55989131+EknoorS@users.noreply.github.com>
2023-12-01 17:47:06 +0100

    Final werkende basic code
    Alle functionaliteiten werken - dit is een eind code -

--- assembly.py
+++ assembly.py
@@ -10,35 +10,18 @@ from picamera2 import Picamera2, Preview
 import time
 from EaseLock_Main import *
 from NumpadMain import *
+import bcrypt
  
 
 
 
 
-# functies uit te voeren wanneer deurbel is gedrukt
-def deurbel_pressed_callback():
-    print("deurbel gedrukt!")
-    picam2 = Picamera2()
-    
-    picam2.start_and_capture_files('foto.jpg', preview_mode="Preview.NULL")
-
-    print("foto getrokken")
-    # Define a unique filename for your uploaded photo
-    now = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
-    filename = "foto_" + now + ".jpg"
-    picam2.close()
-    # Upload the photo to Firebase Storage
-    bucket = storage.bucket()
-    blob = bucket.blob(filename)
-    blob.upload_from_filename('foto.jpg')
-
-    # Laat de database weten dat er net aangebeld werd
-    db.reference('/readtest').set("Er werd het laatst aangebeld op" + datetime.now().strftime("%d-%m-%Y, %H:%M:%S"))
 
 # Set the GPIO mode and pin number
 GPIO.setmode(GPIO.BOARD)
 BUTTON_GPIO = 11  # You can use a different GPIO pin
 LOCK_GPIO = 8
+BOX_GPIO = 8
 
 # Setup the button pin as an input with a pull-up resistor
 GPIO.setup(BUTTON_GPIO, GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
@@ -46,12 +29,21 @@ GPIO.setup(LOCK_GPIO, GPIO.OUT)
 
 # Initialize a variable to keep track of the button state
 previous_state = GPIO.input(BUTTON_GPIO)
+
+# functies uit te voeren wanneer deurbel is gedrukt
+def deurbel_pressed_callback():
+    print("deurbel gedrukt!")
+    foto_trekken_en_uploaden()
+    # Laat de database weten dat er net aangebeld werd
+    db.reference('/readtest').set("Er werd het laatst aangebeld op" + datetime.now().strftime("%d-%m-%Y, %H:%M:%S"))
+
 # Deurbel drukknop monitor
 def button_monitor():
     global previous_state
     while True:
         GPIO.setmode(GPIO.BOARD)
         current_state = GPIO.input(BUTTON_GPIO)
+        print(current_state)
         if current_state != previous_state:
             if current_state == GPIO.LOW:
                 deurbel_pressed_callback()
@@ -69,18 +61,34 @@ def slot(state):
         time.sleep(5)
         print("slot gaat toe")
         GPIO.output(LOCK_GPIO, GPIO.LOW)
-        db.reference('/Locked').set("True")
+        db.reference('/Locked').set(True)
         time.sleep(1)
     elif state:
         print(f"slot gaat toe: {state} (elif satement) ")
         GPIO.output(LOCK_GPIO, GPIO.LOW)
 
+def slot_box(state):
+    state = bool(state)
+    print("slot functie gaat runnen met state: ", state)
+    if  state:
+        print("box gaat open")
+        GPIO.output(BOX_GPIO, GPIO.HIGH)
+        time.sleep(5)
+        print("box gaat toe")
+        GPIO.output(BOX_GPIO, GPIO.LOW)
+        db.reference('/BoxUnlock').set(False)
+        time.sleep(1)
+    elif not state:
+        print(f"box gaat toe: {state} (elif satement) ")
+        GPIO.output(BOX_GPIO, GPIO.LOW)
+
 # Functie die opgeroepen word als de deur ontgrendeld moet worden
 def on_data_change_lock(event):
     # laat weten dat we de change in slot status hebben geregistreerd
-    db.reference('/readtest').set(f"succesvol change ({event.data}) gelezen op: " + datetime.now().strftime("%d-%m-%Y, %H:%M:%S"))
-    # open slot
-    slot(event.data)
+    if not event.data:
+        db.reference('/readtest').set(f"succesvol change ({event.data}) gelezen op: " + datetime.now().strftime("%d-%m-%Y, %H:%M:%S"))
+        #open slot
+        slot(event.data)
 
 # THREAD FUNCTION die kijkt of de deur al dan niet geunlocked moet worden
 def readlock_monitor():
@@ -109,6 +117,7 @@ def cardreader_monitor():
                     CheckTimeslot(GetUserByID(id, newID))
                 else:
                     print('addnewkey')
+                    foto_trekken_en_uploaden()
                     AddNewKey(id, currentID)
                 AddEntry(GetUsername(user) if user is not None else 'Unauthorized')
         except:
@@ -129,14 +138,51 @@ def numpad_reader_monitor():
     finally:
         keypad.cleanup()
 
-
-
+def capture_picture_button_monitor():
+    db.reference('/capture').listen(on_data_change_capture_picture)
+    
+def on_data_change_capture_picture(event):
+    if event.data:
+        foto_trekken_en_uploaden()
+        ref = db.reference('/capture')
+        ref.set(False)
+    
+def unlock_box_monitor():
+    db.reference('/BoxUnlock').listen(on_data_change_box_unlock)
+    
+def on_data_change_box_unlock(event):
+    if event.data:
+        slot_box(event.data)
+    
+        
+        
+def foto_trekken_en_uploaden():
+    picam2 = Picamera2()
+    picam2.start_and_capture_files('foto.jpg', preview_mode="Preview.NULL")
+    print("foto getrokken")
+    # Define a unique filename for your uploaded photo
+    now = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+    filename = "foto_" + now + ".jpg"
+    picam2.close()
+    # Upload the photo to Firebase Storage
+    bucket = storage.bucket()
+    blob = bucket.blob(filename)
+    blob.upload_from_filename('foto.jpg')
+    now = datetime.now()
+    key = now.strftime('%Y-%m-%d') + '_' + now.strftime('%H-%M-%S')
+    ref = db.reference(f'/Photos/{key}')
+    ref.set(filename)
+    
+capture_picture_button_thread = Process(target=capture_picture_button_monitor)
+unlock_box_thread = Process(target=unlock_box_monitor)
 button_thread = Process(target=button_monitor)
 cardreader_thread = Process(target=cardreader_monitor)
 numpad_reader_thread = Process(target=numpad_reader_monitor)
 readlock_monitor_thread = Process(target = readlock_monitor)
 
 try:
+    unlock_box_thread.start()
+    capture_picture_button_thread.start()
     readlock_monitor_thread.start()
     button_thread.start()
     numpad_reader_thread.start()
